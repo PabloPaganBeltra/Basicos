9cc6592fc3f782b961b0f02c66e1154df587fbdc
Pablo Pagán <pablo23clase@gmail.com>
2019-12-02 19:48:27 +0100

    Método numericos con definición de variables

--- src/basicospablopagán/numericos.java
+++ src/basicospablopagán/numericos.java
@@ -23,11 +23,24 @@ public class numericos {
         //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
+        elMasPeque = 3;
+        chiquitin = 10;
+        duplicaEntero = 100;
+        grande = 1;
 
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
         System.err.println("Valor entero");
         System.out.println(entero);
         System.out.println("Valor doble");
         System.out.println(doble);
+        System.out.println("Valor decimal pequeño");
+        System.out.println(grande);
+        System.out.println("Valor entero largo");
+        System.out.println(duplicaEntero);
+        System.out.println("Valor entero pequeño");
+        System.out.println(chiquitin);
+        System.out.println("Valor más pequeño");
+        System.out.println(elMasPeque);
+        
     }
 }
