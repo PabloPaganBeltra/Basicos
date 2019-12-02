b5bf60b3c12b9f5f0138c26b21379c1bd6b4ad9d
Pablo Pagán <pablo23clase@gmail.com>
2019-12-02 19:50:01 +0100

    Creación de cadenas tipo String

--- src/basicospablopagán/numericos.java
+++ src/basicospablopagán/numericos.java
@@ -9,15 +9,19 @@ public class numericos {
         long duplicaEntero;
         float grande;
         double doble;
+        String cadena;
 
         //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
+        cadena = "Esto es un String";
 
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
         System.err.println("Valor entero");
         System.out.println(entero);
         System.out.println("Valor doble");
         System.out.println(doble);
+        System.out.println("Valor String");
+        System.out.println(cadena);
     }
 }
