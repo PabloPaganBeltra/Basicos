802eca0673d23d66148cb7e605a81b858a3a439a
Pablo Pagán <pablo23clase@gmail.com>
2019-12-02 19:40:30 +0100

    Numericos.Creación de rama numericos.

---
+++ src/Lamain.java
@@ -0,0 +1,6 @@
+import static basicospablopagán.numericos.numericos;
+public class Lamain {
+    public static void main(String[] args) {
+        numericos();  // Llamada al método
+    }
+}
---
+++ src/basicospablopagán/numericos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicospablopagán;
+
+/**
+ *
+ * @author Pablo PC
+ */
+public class numericos {
+   public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
+}
