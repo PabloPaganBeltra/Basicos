5842e498c057359dc5f486149e38977309186631
Pablo Pagán <pablo23clase@gmail.com>
2019-12-02 19:43:22 +0100

    Crear rama logicos

--- src/basicospablopagán/numericos.java
+++ src/basicospablopagán/numericos.java
@@ -1,8 +1,6 @@
 package basicospablopagán;
 public class numericos {
    public static void numericos() {
-
-        //Estos son los tipos básicos
         byte elMasPeque;
         short chiquitin;
         int entero;
@@ -10,11 +8,9 @@ public class numericos {
         float grande;
         double doble;
 
-        //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
 
-        //Mostrar en pantalla el contendido de las variables de tipos básicos.
         System.err.println("Valor entero");
         System.out.println(entero);
         System.out.println("Valor doble");
